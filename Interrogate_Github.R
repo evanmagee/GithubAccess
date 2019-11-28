8babc3a124273670b4d846f4e09f50587bd69a8a
Evan Magee <mageeev@tcd.ie>
2019-11-28 11:20:51 +0000

    added for loop that loops through repositories of users

--- Interrogate_Github.R
+++ Interrogate_Github.R
@@ -166,4 +166,25 @@ for (i in 1:length(users))
   RepositoriesContent = content(Repositories)
   RepositoriesDF = jsonlite::fromJSON(jsonlite::toJSON(RepositoriesContent))
   RepositoriesNames = RepositoriesDF$name
+  #Loop through all the repositories of a user
+  for (j in 1: length(RepositoriesNames))
+  {
+   
+    RepositoriesUrl2 = paste("https://api.github.com/repos/", users[i], "/", RepositoriesNames[j], sep = "")
+    Repositories2 = GET(RepositoriesUrl2, gtoken)
+    RepositoriesContent2 = content(Repositories2)
+    RepositoriesDF2 = jsonlite::fromJSON(jsonlite::toJSON(RepositoriesContent2))
+    language = RepositoriesDF2$language
+    
+
+    if (length(language) != 0 && language != "<NA>")
+    {
+      languages[length(languages)+1] = language
+    }
+    next
+  }
+  next
 }
+
+
+
